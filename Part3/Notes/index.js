ac5a1ffa66a5726d96c9ef0daf5a84521165c061
Karen <A23216367@alumnos.uady.mx>
2026-04-27 16:13:00 -0600

    front-bakc

--- Part3/Notes/index.js
+++ Part3/Notes/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
 
 let notes = [
     {
@@ -73,7 +87,13 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
