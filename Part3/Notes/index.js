941c1085bea5c0c5c34584a3c6bb528dbe35a50e
Karen <A23216367@alumnos.uady.mx>
2026-05-08 17:10:27 -0600

    a

--- Part3/Notes/index.js
+++ Part3/Notes/index.js
@@ -96,21 +96,19 @@ const generateId = () => {
     return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ error: 'content missing' })
-    }
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
+    note.save()
+    .then(savedNote => {
         response.json(savedNote)
     })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -124,7 +122,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
 
     next(error)
 }
